2b852b5b741d2a1dd5fd97220e90ddebd0d53386
j-karlsson-marketing <150690198+j-karlsson-marketing@users.noreply.github.com>
2024-11-15 18:21:02 +0000

    a

--- arquetiposanonimos/config/web.php
+++ arquetiposanonimos/config/web.php
@@ -48,6 +48,7 @@ $config = [
             'rules' => [
                 '/' => 'site/index',
                 '/app' => 'site/app',
+                '/app/' => 'site/app',
                 //https://arquetiposanonimos.crom.live/app/view/boas-vindas_comece-aqui
                 '/app/view/<file_name>' => 'site/view',
             ],
