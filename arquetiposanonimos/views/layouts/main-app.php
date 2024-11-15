62ffcadb134d3f99395d5f6868ef224d79dde1c9
j-karlsson-marketing <150690198+j-karlsson-marketing@users.noreply.github.com>
2024-11-15 19:15:24 +0000

    a

--- arquetiposanonimos/views/layouts/main-app.php
+++ arquetiposanonimos/views/layouts/main-app.php
@@ -81,6 +81,7 @@ $this->registerMetaTag(['name' => 'viewport', 'content' => 'width=device-width, 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+    <?php $this->endBody() ?>
 </body>
 
 </html>
