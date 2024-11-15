50b74e6550ff71f5645d1c8a4cf5e14e7f6b5749
j-karlsson-marketing <150690198+j-karlsson-marketing@users.noreply.github.com>
2024-11-15 05:09:55 +0000

    a

--- arquetiposanonimos/views/site/index.php
+++ arquetiposanonimos/views/site/index.php
@@ -12,8 +12,8 @@
 
 <!-- Sobre -->
 <section class="about-section text-center" id="about">
-    <div class="container px-4 px-lg-5">
-        <div class="row gx-4 gx-lg-5 justify-content-center">
+    <div class=" px-4 px-lg-5">
+        <div class="container row gx-4 gx-lg-5 justify-content-center">
             <div class="col-lg-8">
                 <h2 class="text-white mb-4">Sobre o Projeto</h2>
                 <p class="text-white-50">
