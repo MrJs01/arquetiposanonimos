ead64def08109fbdeacfa09a9e4f90d78dc58470
j-karlsson-marketing <150690198+j-karlsson-marketing@users.noreply.github.com>
2024-11-15 18:08:58 +0000

    a

--- arquetiposanonimos/views/layouts/main-app.php
+++ arquetiposanonimos/views/layouts/main-app.php
@@ -27,6 +27,8 @@ $this->registerMetaTag(['name' => 'viewport', 'content' => 'width=device-width, 
         .card { background-color: #222; border: none; }
         .swiper-slide { display: flex; justify-content: center; align-items: center; }
     </style>
+    <!-- jquery -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 </head>
 
 <body>
