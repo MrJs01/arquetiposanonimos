31f0e55aa36585a933b3a67954046eab7e58f7b2
j-karlsson-marketing <150690198+j-karlsson-marketing@users.noreply.github.com>
2024-11-15 05:34:06 +0000

    a

--- arquetiposanonimos/views/site/index.php
+++ arquetiposanonimos/views/site/index.php
@@ -29,7 +29,7 @@
 <section class="projects-section bg-light" id="projects">
     <div class="container px-4 px-lg-5">
         <!-- Projeto Destaque -->
-        <div class="row gx-0 mb-4 mb-lg-5 align-items-center">
+        <div class="row gx-0 mb-4 mb-lg-5 align-items-center bg-faded">
             <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0" src="/assets_main/img/wal4.jpeg" alt="Imagem de Arquétipos" /></div>
             <div class="col-xl-4 col-lg-5">
                 <div class="featured-text text-center text-lg-left">
